ba19360518330ab7b653895dfb4b32f334cf3632
odouimi <o.douimi@attijariwafa.com>
2025-05-24 13:25:43 +0100

    headless mode

--- app/services/report_service.py
+++ app/services/report_service.py
@@ -39,7 +39,7 @@ class ReportGenerationService:
     def _initialize_driver(self):
         """Initializes the Selenium WebDriver."""
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--window-size=1920,1080")
